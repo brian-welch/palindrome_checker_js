f0b6f55207842add58c0188f1582fb953da56328
Brian Welch <myself@brianwelch.se>
2018-09-27 07:09:25 +0200

    Uploaded
    Simple sample text in string

---
+++ palindrome_check.js
@@ -0,0 +1,19 @@
+const str = 'oiruygyuri';
+const arr = str.split('');
+const palCheck = (input) => {
+  let length = input.length;
+  let iter = (Math.floor(length/2));
+  let res = 'It\'s a palindrome!';
+  let iterations = 0;
+  for (let i = 0; i < iter; i++) {
+    iterations++;
+    if (input[i] !== input[(length - (i+1))]){
+      res = "It's not a palindrome!";
+      break;
+    }
+  }
+  console.log('Iterations: ' + iterations);
+  console.log(res);
+}
+
+palCheck(arr);
