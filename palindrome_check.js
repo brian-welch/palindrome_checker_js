70dc8cfa6b43457e22b6c7933af6980ffd6f877d
Brian Welch <myself@brianwelch.se>
2018-10-14 09:48:08 +0200

    Some teeny tiny refactoring and variable tweeks

--- palindrome_check.js
+++ palindrome_check.js
@@ -1,18 +1,17 @@
 const str = 'oiruygyuri'; // Given sample string to analyze
 const arr = str.split('');
 const palCheck = (input) => {
-  let length = input.length;
-  let iter = (Math.floor(length/2));
+  let max_iter = (Math.floor((input.length)/2));
   let res = 'It\'s a palindrome!';
-  let iterations = 0;
-  for (let i = 0; i < iter; i++) {
-    iterations++;
-    if (input[i] !== input[(length - (i+1))]){
+  let iter = 0;
+  for (let i = 0; i < max_iter; i++) {
+    iter++;
+    if (input[i] !== input[(input.length - (i+1))]){
       res = "It's not a palindrome!";
       break;
     }
   }
-  console.log('Iterations: ' + iterations);
+  console.log('Iterations: ' + iter);
   console.log(res);
 }
 
