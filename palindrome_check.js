140c49c9159d381bdbdb1c9c23dde196f39a83d0
Brian Welch <myself@brianwelch.se>
2019-06-07 13:40:32 +0200

    Refactored code to accept string and break into array in function block

--- palindrome_check.js
+++ palindrome_check.js
@@ -1,12 +1,12 @@
 const str = 'oiruygyuri'; // Given sample string to analyze
-const arr = str.split('');
 const palCheck = (input) => {
-  let max_iter = (Math.floor((input.length)/2));
+  let arr = input.split('');
+  let max_iter = (Math.floor((arr.length)/2));
   let res = 'It\'s a palindrome!';
   let iter = 0;
   for (let i = 0; i < max_iter; i++) {
     iter++;
-    if (input[i] !== input[(input.length - (i+1))]){
+    if (arr[i] !== input[(arr.length - (i+1))]){
       res = "It's not a palindrome!";
       break;
     }
@@ -15,4 +15,4 @@ const palCheck = (input) => {
   console.log(res);
 }
 
-palCheck(arr);
+palCheck(str);
